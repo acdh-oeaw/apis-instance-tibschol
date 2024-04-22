ca23c375e1af5d1dcf77282efd17cb1e123660e4
Gytha Ogg <gythaoggscat@gmail.com>
2024-04-22 08:11:31 +0200

    Parameterise importing entity data

--- apis_ontology/management/commands/import_entities.py
+++ apis_ontology/management/commands/import_entities.py
@@ -1,14 +1,14 @@
 import logging
-from pprint import pprint
 
 import pandas as pd
-from apis_ontology.models import Place
 from django.apps import apps
 from django.core.management.base import BaseCommand
 from tqdm.auto import tqdm
 
 logger = logging.getLogger(__name__)
 pd.set_option("display.max_colwidth", None)
+
+ENTITY_MODELS = ["person", "place", "instance", "work"]
 
 
 class Command(BaseCommand):
@@ -18,35 +18,43 @@ class Command(BaseCommand):
 
     import_file = "data/dump_test.json"
 
+    def add_arguments(self, parser):
+        parser.add_argument("args", nargs="*", type=str)
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
         self.df = pd.read_json(self.import_file)
         self.df = self.df[self.df.model != "apis_metainfo.uri"]
         logger.debug("Columns: %s", self.df.columns)
         logger.debug("Number of rows: %d", self.df.shape[0])
         logger.debug("Models: %s", self.df.model.unique())
-        Place.objects.all().delete()
 
     def handle(self, *args, **kwargs):
         """
         parses the data dump from the old schema and imports it into the current schema
         """
+        if not args:
+            logging.error("Please enter the names of entity models to import")
+            return
 
         def get_entity_data(model_name):
-            ENTITY_MODELS = ["person", "place", "instance", "work"]
             if model_name not in ENTITY_MODELS:
                 logging.error(
                     "%s is unknown. Expecting one of %s", model_name, ENTITY_MODELS
                 )
-            enitity_rows = self.df[self.df.model == f"apis_ontology.{model_name}"]
-            logging.debug("Found %d rows of type %s", enitity_rows.shape[0], model_name)
+            entity_rows = self.df[self.df.model == f"apis_ontology.{model_name}"]
+            logging.debug("Found %d rows of type %s", entity_rows.shape[0], model_name)
             other_fields = {}
-            for _, row in tqdm(enitity_rows.iterrows(), total=enitity_rows.shape[0]):
+            for _, row in tqdm(entity_rows.iterrows(), total=entity_rows.shape[0]):
                 # get data from root object
                 other_object_info = self.df[
                     (self.df.pk == row.pk) & (self.df.model != row.model)
                 ]
                 for _, extra_fields in other_object_info.iterrows():
+                    if extra_fields.model.startswith("apis_ontology"):
+                        continue
+
                     other_fields = {**other_fields, **extra_fields.fields}
 
                 field_values = {
@@ -72,4 +80,9 @@ class Command(BaseCommand):
                 model_object = model_class(**field_values)
                 model_object.save()
 
-        df = get_entity_data("place")
+        for arg in args:
+            if arg not in ENTITY_MODELS:
+                logging.error("Unrecognised entity type %s for import", arg)
+                break
+
+            get_entity_data(arg)

7eb356e0cd2ae188b97bfb2389759193ec9f970c
Gytha Ogg <gythaoggscat@gmail.com>
2024-04-23 11:39:00 +0200

    Move filter functions to a single file

--- apis_ontology/templatetags/filter_utils.py
+++ apis_ontology/templatetags/filter_utils.py
@@ -1,4 +1,5 @@
 from django import template
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
@@ -7,3 +8,25 @@ register = template.Library()
 def endswith(value, suffix):
     """Custom filter to check if value ends with the specified suffix."""
     return str(value).endswith(suffix)
+
+
+@register.filter
+def render_links(value):
+    if not value:
+        return ""
+
+    links = [link.strip() for link in value.split("\n") if link.strip()]
+    rendered_links = "<br>".join(
+        f'<a href="{link}" target="_blank">{link}</a>' for link in links
+    )
+    return mark_safe(rendered_links)
+
+
+@register.filter
+def render_list_field(value):
+    if not value:
+        return ""
+
+    list_vals = [v.strip() for v in value.split("\n") if v.strip()]
+    rendered_list = "<br>".join(v for v in list_vals)
+    return mark_safe(rendered_list)

f4e8b42acc76070b0c3a39ac6f759a92fd6057f1
Gytha Ogg <gythaoggscat@gmail.com>
2024-05-17 22:59:22 +0200

    instance: link zotero_refs field contents
    Delimiter is either comma or linebreak
    Possible reasons for link showing ID:
     - Zoreto entry not tagged with APIS, hence not cached
     - Recently created Zotero entry, not yet cached
     - Missing ShortTitle attribute in the Zotero entry

--- apis_ontology/templatetags/filter_utils.py
+++ apis_ontology/templatetags/filter_utils.py
@@ -1,7 +1,11 @@
+import logging
+from apis_ontology.models import ZoteroEntry
 from django import template
 from django.utils.safestring import mark_safe
 
 register = template.Library()
+
+logger = logging.getLogger(__name__)
 
 
 @register.filter
@@ -38,3 +42,35 @@ def render_coordinate(value):
         return round(value, 4)
     except:
         return ""
+
+
+@register.filter
+def render_zotero_links(value):
+    if not value:
+        return ""
+
+    delim = "\n" if "\n" in value else ","
+    zotero_refs = [
+        zotero_id.strip().replace("[", "").replace("]", "")
+        for zotero_id in value.split(delim)
+        if zotero_id.strip()
+    ]
+    links = []
+    for zotero_id in zotero_refs:
+        link_text = zotero_id  # fallback default value
+        try:
+            zotero_obj = ZoteroEntry.objects.filter(zoteroId=zotero_id)[0]
+            link_text = zotero_obj.shortTitle if zotero_obj.shortTitle else zotero_id
+
+        except IndexError:
+            logger.error(f"Error finding cached Zotero entry with ID %s", zotero_id)
+        except Exception as e:
+            logger.error(
+                f"Error while fetching zotero data %s\n for %s", repr(e), zotero_id
+            )
+        links.append(
+            f'<a target="_BLANK" href="https://www.zotero.org/groups/4394244/tibschol/items/{zotero_id}/item-details#">{link_text}</a>'
+        )
+
+    rendered_links = "<br>".join(links)
+    return mark_safe(rendered_links)

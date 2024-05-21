95b68386e5f11f5629450cef6ab858218e65ecf7
Gytha Ogg <gythaoggscat@gmail.com>
2024-05-21 20:44:59 +0200

    show short form of citation for instances

--- apis_ontology/templatetags/citation.py
+++ apis_ontology/templatetags/citation.py
@@ -17,15 +17,9 @@ def cite(object):
     print(object.set_num, object.pp_kdsb, object.volume)
     print(citations[object.set_num])
     zotero_info = citations[object.set_num]
-
-    title = zotero_info["data"]["title"]
+    short_title = zotero_info["data"].get("shortTitle", "")
+    if not short_title.strip():
+        short_title = zotero_info["key"]
     key = zotero_info["key"]
-    creator = zotero_info["data"]["creators"][0]["lastName"]
-    constant = "[dPe sgrig 'gan 'khur ba: dByang can lha mo et al.], Chengdu"
-    place = zotero_info["data"]["place"]
-    publisher = zotero_info["data"]["publisher"]
-    date = zotero_info["data"]["date"]
-    link = f"""<a target="_BLANK" href="https://www.zotero.org/groups/4394244/tibschol/items/{key}/item-details#">{title}</a>"""
-    return mark_safe(
-        f"""{link}, vol. {object.volume}, {creator} {constant} [{place}]: {publisher}, {date}, pp. {object.pp_kdsb}"""
-    )
+    link = f"""<a target="_BLANK" href="https://www.zotero.org/groups/4394244/tibschol/items/{key}/item-details#">{short_title}</a>"""
+    return mark_safe(f"""{link}, vol. {object.volume}, {object.pp_kdsb}""")

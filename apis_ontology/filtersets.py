aaa3d9470d6d6e177d6c363f8225a0f4a9841059
Gytha Ogg <gythaoggscat@gmail.com>
2024-06-03 02:54:50 +0200

    Custom filterset for Place

--- apis_ontology/filtersets.py
+++ apis_ontology/filtersets.py
@@ -2,6 +2,7 @@ from apis_core.apis_entities.filtersets import (
     AbstractEntityFilterSet,
     ABSTRACT_ENTITY_FILTERS_EXCLUDE,
 )
+from apis_ontology.forms import PlaceSearchForm
 from django.db import models
 import django_filters
 
@@ -31,6 +32,7 @@ class LegacyStuffMixinFilterSet(AbstractEntityFilterSet):
 
 class TibScholEntityMixinFilterSet(AbstractEntityFilterSet):
     class Meta(AbstractEntityFilterSet.Meta):
+        exclude = ABSTRACT_ENTITY_FILTERS_EXCLUDE
         filter_overrides = {
             models.CharField: {
                 "filter_class": django_filters.CharFilter,
@@ -45,3 +47,49 @@ class TibScholEntityMixinFilterSet(AbstractEntityFilterSet):
                 },
             },
         }
+
+    name = django_filters.CharFilter(method="custom_name_search")
+
+    comments = django_filters.CharFilter(
+        label="Comments contain", lookup_expr="icontains"
+    )
+    external_links = django_filters.CharFilter(
+        label="External links contain", lookup_expr="icontains"
+    )
+    alternative_names = django_filters.CharFilter(
+        label="Alternative names contain", lookup_expr="icontains"
+    )
+
+    def custom_name_search(self, queryset, name, value):
+        return queryset.filter(
+            models.Q(name__icontains=value)
+            | models.Q(alternative_names__icontains=value)
+        )
+
+
+class PlaceFilterSet(TibScholEntityMixinFilterSet):
+    class Meta:
+        exclude = [*ABSTRACT_ENTITY_FILTERS_EXCLUDE, "latitude", "longitude", "notes"]
+        form = PlaceSearchForm
+        filter_overrides = {
+            models.CharField: {
+                "filter_class": django_filters.CharFilter,
+                "extra": lambda f: {
+                    "lookup_expr": "icontains",
+                },
+            },
+            models.TextField: {
+                "filter_class": django_filters.CharFilter,
+                "extra": lambda f: {
+                    "lookup_expr": "icontains",
+                },
+            },
+        }
+
+    label = django_filters.CharFilter(method="custom_name_search")
+
+    def custom_name_search(self, queryset, name, value):
+        return queryset.filter(
+            models.Q(label__icontains=value)
+            | models.Q(alternative_names__icontains=value)
+        )

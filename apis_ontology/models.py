102a28ac8b370c12b61b6874b9c67ac8c4d86ed6
Gytha Ogg <gythaoggscat@gmail.com>
2024-07-16 07:44:28 +0200

    remodel (instance): introduce dimension field

--- apis_ontology/models.py
+++ apis_ontology/models.py
@@ -296,6 +296,9 @@ class Instance(
         null=True,
         verbose_name="Availability",
     )
+    dimension = models.CharField(
+        max_length=255, blank=True, null=True, verbose_name="WxH size in cm"
+    )
     item_description = models.TextField(
         blank=True, null=True, verbose_name="Item description"
     )

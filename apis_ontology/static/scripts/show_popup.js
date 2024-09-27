9dca7e4cc0ea09cbd4c3d72d42ffad132b6e2a4c
Gytha Ogg <gythaoggscat@gmail.com>
2024-09-27 12:12:31 +0200

    Experimental styles for TEI

--- apis_ontology/static/scripts/show_popup.js
+++ apis_ontology/static/scripts/show_popup.js
@@ -1,14 +1,72 @@
+let c = new CETEI({
+  ignoreFragmentId: true
+});
+let behaviors = {
+  "tei": {
+    "head": function(e) {
+      let level = document.evaluate("count(ancestor::tei-div)", e, null, XPathResult.NUMBER_TYPE, null);
+      let result = document.createElement("h" + (level.numberValue > 7 ? 7 : level.numberValue));
+      for (let n of Array.from(e.childNodes)) {
+        result.appendChild(n.cloneNode());
+      }
+      return result;
+    },
+    "lb": ["<br/><span class=\"break\">$@n&nbsp;</span>"],
+    "pb": ["<p class=\"break\">$@n</p>"],
+
+    // Hyperlink the rs tag and include type and ref in its title
+    "rs": function(e) {
+      let result = document.createElement("a");
+      result.setAttribute("href", "/apis/apis_ontology."+e.getAttribute("type")+"/"+e.getAttribute("ref").split(":")[1]);
+      result.setAttribute("title", "type: " + e.getAttribute("type") + ", id: " + e.getAttribute("ref"));
+      for (let n of Array.from(e.childNodes)) {
+        result.appendChild(n.cloneNode());
+      }
+      return result;
+    },
+
+    // Handle the seg element
+    "seg": function(e) {
+      let result = document.createElement("div"); // Use a <div> for segments
+      result.classList.add("seg");
+
+      // Process child nodes and append
+      for (let n of Array.from(e.childNodes)) {
+        // Call the appropriate behavior for each child node
+        let behavior = behaviors.tei[n.nodeName.toLowerCase()];
+        if (behavior) {
+          result.appendChild(behavior(n)); // Process child nodes based on their type
+        } else {
+          result.appendChild(n.cloneNode(true)); // Clone nodes that don't have specific behavior
+        }
+      }
+      return result;
+    },
+  }
+};
+
+c.addBehaviors(behaviors);
+
 function showPopup(recordId, renderStyle) {
+  document.getElementById("popupContent").innerHTML = "";
   renderStyle = "tei";
   fetch(`/apis/excerpts/${recordId}/${renderStyle}`)
     .then(response => response.text())
-    .then(data => {
-      // Display the data in the popup
-      document.getElementById('popupContent').innerHTML = data;
-      document.getElementById('popupModal').style.display = 'block';
-    })
-    .catch(error => {
-      console.error('Error fetching dynamic content:', error);
+    .then(teidata => {
+      // show 404 error if the respose is 404
+      if (teidata.includes("Page not found")) {
+        document.getElementById("popupContent").innerHTML = "Excerpt Not Found";
+        document.getElementById('popupModal').style.display = 'block';
+        return;
+      }
+      // show raw teidata in the element #rawTEI as text
+      document.getElementById("rawTEI").innerText = teidata;
+      console.log(teidata);
+      c.makeHTML5(teidata, function(data) {
+        document.getElementById("popupContent").appendChild(data);
+        document.getElementById('popupModal').style.display = 'block';
+      });
+
     });
 }
 

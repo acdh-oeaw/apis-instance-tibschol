e2760cc154123ad61d58e1f578b59099e89dc844
Gytha Ogg <gythaoggscat@gmail.com>
2024-12-03 11:13:00 +0100

    remove base url config

--- apis_ontology/settings/server_settings.py
+++ apis_ontology/settings/server_settings.py
@@ -3,7 +3,6 @@ from apis_acdhch_default_settings.settings import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-APIS_BASE_URI = "https://tibschol.acdh-ch.oeaw.ac.at/"
 
 ROOT_URLCONF = "apis_ontology.urls"
 CSRF_TRUSTED_ORIGINS = [

d6b416bfbeca3905713bb5547ce4a26b683b1eb2
Gytha Ogg <gythaoggscat@gmail.com>
2025-02-07 11:37:54 +0100

    tables: use responsive striped/hover template

--- apis_ontology/settings/server_settings.py
+++ apis_ontology/settings/server_settings.py
@@ -69,3 +69,7 @@ MIDDLEWARE += [
 
 APIS_ANON_VIEWS_ALLOWED = False
 EXPORT_FORMATS = ()
+DJANGO_TABLES2_TEMPLATE = "django_tables2/bootstrap5-responsive.html"
+DJANGO_TABLES2_TABLE_ATTRS = {
+    "class": "table table-striped table-hover",
+}

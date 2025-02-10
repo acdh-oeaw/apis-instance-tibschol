d6a2f2e9239d824d144257081bc1997d559b56a0
Gytha Ogg <gythaoggscat@gmail.com>
2025-02-10 10:20:49 +0100

    Handle null values of Char field in __str__ function

--- apis_ontology/models.py
+++ apis_ontology/models.py
@@ -23,7 +23,7 @@ class Subject(GenericModel, models.Model):
     name = models.CharField(max_length=255, unique=True)
 
     def __str__(self):
-        return self.name
+        return f"{self.name or ''}"
 
     class Meta:
         verbose_name = _("Topic")

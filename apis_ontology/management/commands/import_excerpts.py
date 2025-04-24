2e84a97a654f8eb4711fdc09a3397fc6be3f93de
Gytha Ogg <gythaoggscat@gmail.com>
2025-04-24 17:46:35 +0200

    management: parameterise excerpts import file

--- apis_ontology/management/commands/import_excerpts.py
+++ apis_ontology/management/commands/import_excerpts.py
@@ -13,15 +13,23 @@ class Command(BaseCommand):
     Imports excerpts from data/excerpts.csv
     """
 
+    def add_arguments(self, parser):
+        parser.add_argument("dump", type=str, help="dump file name")
+
     def handle(self, *args, **kwargs):
         def create_record(row):
             record, _ = Excerpts.objects.get_or_create(xml_id=row.xml_id)
             record.__dict__.update(row.to_dict())
             record.save()
 
-        print(len(Excerpts.objects.all()))
-        import_file = "data/excerpts.csv"
+        # define an input parameter for import_file
+
+        print(f"Found {len(Excerpts.objects.all())} excerpts in the database")
+        import_file = kwargs["dump"]
+
         df = pd.read_csv(import_file).fillna("")
+        print(f"Importing {df.shape[0]} excerpts from {import_file}")
+
         for _, row in tqdm(df.iterrows(), total=df.shape[0]):
             create_record(row)
 

7c16828786bb15410a4c10c5b643828450797c14
Gytha Ogg <gythaoggscat@gmail.com>
2025-05-16 11:35:02 +0200

    fix(excerpts): remove unreferenced excerpts from excerpts-in-use collection

--- apis_ontology/management/commands/excerpts-in-use-collection.py
+++ apis_ontology/management/commands/excerpts-in-use-collection.py
@@ -19,15 +19,21 @@ class Command(BaseCommand):
         for excerpt in tqdm(Excerpts.objects.all()):
             # check in xml_id is present in the tei_refs column
             # of Relation or in any of its subclasses at least once
+            in_use = False
             for rel_subclass in TibScholRelationMixin.__subclasses__():
                 if rel_subclass.objects.filter(
                     tei_refs__contains=excerpt.xml_id
                 ).exists():
                     excerpt_collection.add(excerpt)
+                    in_use = True
                     break
-
+            if not in_use:
+                try:
+                    excerpt_collection.remove(excerpt)
+                except Exception as e:
+                    pass
         self.stdout.write(
             self.style.SUCCESS(
-                "Excerpts have been successfully added to the collection."
+                "Excerpts used as references have been successfully added to experts-in-use collection."
             )
         )

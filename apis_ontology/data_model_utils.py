59ee889065139d2515b5b36710d6e63471db30e5
Gytha Ogg <gythaoggscat@gmail.com>
2025-06-06 03:28:37 +0200

    fix(data-model):include missing reverse relations
    closes #382

--- apis_ontology/data_model_utils.py
+++ apis_ontology/data_model_utils.py
@@ -3,11 +3,10 @@ from apis_core.apis_entities.utils import get_entity_content_types
 from apis_core.relations.utils import relation_content_types
 
 
-class DataModel:
-    entities: list = []
-    relations: list = []
-    matrix = defaultdict(dict)
+from collections import defaultdict
 
+
+class DataModel:
     def __init__(self):
         self.entities = get_entity_content_types()
         self.relations = relation_content_types()
@@ -15,27 +14,37 @@ class DataModel:
 
         for rel in self.relations:
             rel_model = rel.model_class()
-            if (
-                str(rel_model.subj_model.__name__)
-                in self.matrix[str(rel_model.obj_model.__name__)]
-            ):
-                self.matrix[str(rel_model.subj_model.__name__)][
-                    str(rel_model.obj_model.__name__)
-                ].append(rel_model.name())
+            subj = str(rel_model.subj_model.__name__)
+            obj = str(rel_model.obj_model.__name__)
+            name = rel_model.name()
+            rev_name = rel_model.reverse_name()
 
-                self.matrix[str(rel_model.obj_model.__name__)][
-                    str(rel_model.subj_model.__name__)
-                ].append(rel_model.reverse_name())
+            # initialisethe matrix if not already done
+            if subj not in self.matrix:
+                self.matrix[subj] = {}
+            if obj not in self.matrix[subj]:
+                self.matrix[subj][obj] = []
 
-            else:
-                self.matrix[str(rel_model.subj_model.__name__)][
-                    str(rel_model.obj_model.__name__)
-                ] = [rel_model.name()]
-                self.matrix[str(rel_model.obj_model.__name__)][
-                    str(rel_model.subj_model.__name__)
-                ] = [rel_model.reverse_name()]
+            if obj not in self.matrix:
+                self.matrix[obj] = {}
+            if subj not in self.matrix[obj]:
+                self.matrix[obj][subj] = []
+
+            # if subj and obj are the same then add name and reverse in the same row
+            if subj == obj:
+                if name == rev_name:
+                    self.matrix[subj][obj].append(name)
+                    continue
+
+                self.matrix[subj][obj].append(
+                    f"{name}<span class='material-symbols-outlined align-middle text-secondary'>arrows_outward</span>{rev_name}"
+                )
+                continue
+
+            self.matrix[subj][obj].append(name)
+            self.matrix[obj][subj].append(rev_name)
 
         # sort the relations
         for row in self.matrix:
             for col in self.matrix[row]:
-                self.matrix[row][col] = sorted(list(set(self.matrix[row][col])))
+                self.matrix[row][col] = sorted(set(self.matrix[row][col]))

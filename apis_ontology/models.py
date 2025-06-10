084c596a05e1847193482c698532215c6f731a7e
Gytha Ogg <gythaoggscat@gmail.com>
2025-06-10 10:22:57 +0200

    feat: change verbose name for field Nationality to Origin
    addresses #446

--- apis_ontology/models.py
+++ apis_ontology/models.py
@@ -104,7 +104,7 @@ class Person(VersionMixin, LegacyStuffMixin, TibScholEntityMixin, AbstractEntity
     name = models.CharField(max_length=255, blank=True, default="", verbose_name="Name")
     gender = models.CharField(max_length=6, choices=GENDERS, default="male")
     nationality = models.CharField(
-        max_length=10, choices=NATIONALITY, blank=True, null=True
+        max_length=10, choices=NATIONALITY, blank=True, null=True, verbose_name="Origin"
     )
 
     objects = TibScholEntityManager()
